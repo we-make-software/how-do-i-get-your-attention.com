ac4c43e559bc1ab9c2600a1b8cc4b7a00b0c34c4
root <root@B873EE7>
2024-10-11 18:42:37 +0000

    Auto commit

--- Memory.init.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include "Memory.h"
-extern void memoryInit(void);
--- wms.c
+++ wms.c
@@ -1,18 +1,12 @@
 #include "Memory.h"
-
-
 static int __init wmsInit(void) {
-
     printk(KERN_INFO "Hi from the Kernel Module!\n");
     return 0;
 }
-
 static void __exit wmsExit(void) {
 
     printk(KERN_INFO "Goodbye from the Kernel Module!\n");
 }
-
 module_init(wmsInit);
 module_exit(wmsExit);
-
 MODULE_METADATA(); 
