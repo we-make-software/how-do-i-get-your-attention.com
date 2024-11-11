fff3f46f059daf6c1e6241974d9ce25be3243865
root <root@B873EE7>
2024-11-11 05:28:21 +0000

    Auto commit

--- RFC791Software.c
+++ RFC791Software.c
@@ -1,2 +1,4 @@
 #include"RFC791Software.h"
 extern int RFC791In(struct Frame*frame);
+extern void RFC791Setup(struct IEE802_3Functions*iEE802_3);
+MODULE_INFO_SETUP("Pirasath Luxchumykanthan", "WeMakeSoftware RFC791", "1.0");
--- RFC8200Software.c
+++ RFC8200Software.c
@@ -1,3 +1,5 @@
 #include"RFC8200Software.h"
 extern int RFC8200In(struct Frame*frame);
 extern void RFC8200Setup(struct IEE802_3Functions*iEE802_3);
+
+MODULE_INFO_SETUP("Pirasath Luxchumykanthan", "WeMakeSoftware RFC8200", "1.0");
