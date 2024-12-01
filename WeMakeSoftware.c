c49f8f89468c61dd1df8a4206dc2c4a7dfc34b72
root <root@B873EE7>
2024-12-01 15:09:09 +0000

    Auto commit

--- WeMakeSoftware.c
+++ WeMakeSoftware.c
@@ -13,19 +13,8 @@ static inline int DropAndCloseFrame(struct Frame*frame);
 static inline struct Frame*CreateFrame(uint8_t id);
 static inline int SetSizeFrame(struct Frame*frame,uint16_t Size);
 static inline int SendFrame(struct Frame*frame);
-static inline char*GetStandard(struct Frame*frame,uint16_t version,uint16_t section);
-static inline int CloseStandard(struct Frame*frame,uint16_t version,uint16_t section);
-static inline bool CreateStandard(struct Frame*frame,uint16_t version,uint16_t section,char**pointer,int64_t position);
-static inline bool AddStandard(struct Frame*frame,uint16_t version,uint16_t section,char**pointer,char*data,bool IsDynamic);
-static inline int wmsFree(const void *objp);
-static inline int RegisterLink(struct Link*links,uint16_t ID,unsigned char*Data );
-static struct Link*GetLinks(struct Frame*frame,uint16_t version,uint16_t section);
-static inline struct Link* GetLink(struct Link* links, uint16_t ID);
-static inline int CreateJob(int (*task_fn)(void *), void *data,struct Job* jobs);
-static inline int RunJobs(struct Job* Jobs);
 
-
-
+static inline void ShowTimeByFrame(struct Frame*frame);
 
 
 
@@ -37,30 +26,22 @@ static int FrameReader(struct sk_buff*skb,struct net_device*dev,struct packet_ty
         return 1; 
     }
     frame->IEE802Buffer=skb_mac_header(frame->skb=skb);
-   /*
-    while (frame->Status.Request!=2&& ktime_get()<frame->End)
-    {
-        switch (frame->Status.Request)
-        {
-        case 1:{
-            frame->Status.Response=frame->Status.Request;
-            return ITakeControl(); 
-        }
-        case 3:{
-             frame->Status.Response=frame->Status.Request;
-             return DropFrame(frame);
-        }
-        case 4:{
-            frame->Status.Response=frame->Status.Request;
-            return DropAndCloseFrame(frame);
-        }
-        default:
-            ndelay(1);
-            continue;
-        }
-    }*/
+    if(!(frame->IEE802Buffer[0]&3)&&!(frame->IEE802Buffer[6]&3)){
+        Print("Destination MAC",frame->IEE802Buffer,0,5);
+        Print("Souce MAC",frame->IEE802Buffer,6,11);
+        ShowTimeByFrame(frame);  
+        return CloseFrame(frame);  
+    }
+
     return CloseFrame(frame);
 }
+
+static inline void ShowTimeByFrame(struct Frame*frame){
+    pr_info("Time (ns): %lld\n", ktime_to_ns(ktime_sub(ktime_get(), frame->Start)));
+
+}
+
+
 #include<linux/slab.h>
 #include<linux/string.h>
 static void Print(const char *title, const unsigned char *data, int from, int to) {
@@ -84,9 +65,8 @@ static void Print(const char *title, const unsigned char *data, int from, int to
 #include<linux/sysinfo.h>
 static struct sysinfo si;
 static inline bool isMemoryAvailable(unsigned long memoryRequiredBytes);
-static uint64_t threadsmax = 200, threads = 0;
 static inline bool isMemoryAvailable(unsigned long memoryRequiredBytes){
-    if(IsServerClose||threads>threadsmax)return false;
+    if(IsServerClose)return false;
     si_meminfo(&si);
     return((si.freeram*PAGE_SIZE)-4294967296)>=memoryRequiredBytes;
 }
@@ -98,80 +78,35 @@ static inline bool waitForMemoryIsAvailable(unsigned long memoryRequiredBytes){
     }
     return false;
 }
-
-
 static inline void*waitForMemory(unsigned long memoryRequiredBytes){return waitForMemoryIsAvailable(memoryRequiredBytes)?kmalloc(memoryRequiredBytes,GFP_KERNEL):NULL;}
 static struct packet_type Gateway={.type=htons(ETH_P_ALL),.func=FrameReader,.ignore_outgoing=1};
-static inline char*GetStandard(struct Frame*frame,uint16_t version,uint16_t section) {
-    struct Standard*this; 
-    for(this=frame->Standards;this&&!(this->Version==version&&this->Section==section);this=this->Previous);
-    return this?this->Data:NULL;
-}
-static inline int CloseStandard(struct Frame*frame,uint16_t version,uint16_t section){
-    if(!frame||!frame->Standards)return -1; 
-    struct Standard*this;
-    for(this=frame->Standards;this&&!(this->Version==version&&this->Section==section);this=this->Previous);
-    if(!this)return 0; 
-    if(this->Previous)this->Previous->Next=this->Next;
-    if(this->Next)this->Next->Previous=this->Previous;
-    if(frame->Standards==this)frame->Standards=this->Previous;
-    return wmsFree(this->Data)&&wmsFree(this); 
-}
-static inline int wmsFree(const void *objp){
-    if(objp)kfree(objp);
-    return 1; 
-}
-static inline bool CreateStandard(struct Frame*frame,uint16_t version,uint16_t section,char**pointer,int64_t position) {
-    struct Standard*this;
-    for(this=frame->Standards;this&&!(this->Version==version&&this->Section==section);this=this->Previous);
-    if(this||!(this=waitForMemory(sizeof(struct Standard))))return false;
-    this->Version=version;
-    this->Section=section;
-    this->Links=NULL;
-    this->Next=NULL;
-    *pointer=this->Data=frame->IEE802Buffer+position;
-    if((this->Previous=frame->Standards))frame->Standards->Next=this;
-    frame->Standards = this;
-    return true;
-}
-static inline bool AddStandard(struct Frame*frame,uint16_t version,uint16_t section,char**pointer,char*data, bool IsDynamic) {
-    struct Standard*this;
-    for(this=frame->Standards;this&&!(this->Version==version&&this->Section==section);this=this->Previous);
-    if(this||!(this=waitForMemory(sizeof(struct Standard))))return false;
-    this->IsDynamic=IsDynamic;
-    this->Version=version;
-    this->Section=section;
-    this->Next=NULL;
-    *pointer=data;
-    if((this->Previous=frame->Standards))frame->Standards->Next=this;
-    frame->Standards = this;
-    return true;
-}
 
-
-
+static inline int BackgroundDeletion(void*data){
+    while (!kthread_should_stop()) {
+        if (!Frames) {
+            msleep(100);
+            continue;
+        }
+        for (struct Frame *frame = Frames; frame; frame = frame->Previous)
+              if (frame->IsDeleted) {
+                    if(frame==Frames){
+                        Frames=frame->Previous;
+                        if(Frames&&Frames->Next)Frames->Next=NULL;
+                    }else {
+                        if (frame->Previous) 
+                            frame->Previous->Next = frame->Next;
+                        if (frame->Next)
+                            frame->Next->Previous = frame->Previous;
+                    }
+                    kfree(frame);
+                }
+       msleep(10);
+    }
+    return 1;
+}
 static inline int CloseFrame(struct Frame*frame) {
     if(!frame)return 0;
-    if(frame==Frames){
-        Frames=frame->Previous;
-        if(Frames&&Frames->Next)
-            Frames->Next=NULL;
-    }else {
-        if (frame->Previous) 
-            frame->Previous->Next = frame->Next;
-        if (frame->Next)
-            frame->Next->Previous = frame->Previous;
-    }
-    if(frame->Standards)
-      for(struct Standard*this=frame->Standards;this;this=this->Previous) 
-      {
-        if(this->Links)
-          for(struct Link*link=this->Links;link;link=link->Previous) 
-            if(link->Data)kfree(link->Data);
-        if(this->IsDynamic)kfree(this->Data);
-         kfree(this);
-      }
-    kfree(frame);
+    frame->IsDeleted=1;
     return 0;
 }
 static inline int ITakeControl(){
@@ -197,11 +132,9 @@ static inline struct Frame*CreateFrame(uint8_t id) {
         frame->Previous=Frames;
     }else
        frame->Previous=NULL;
-    frame->Standards=NULL;
     frame->id=id;
-    frame->Status.Response=frame->Status.Request=0;
-    frame->End=frame->Start=ktime_get();
-     frame->End = ktime_add(frame->End, ktime_set(10, 0));
+    frame->IsDeleted=0;
+    frame->Start=ktime_get();
     if((frame->Previous=Frames))Frames->Next=frame;
     return Frames=frame;
 }
@@ -278,99 +211,12 @@ if (result < 0) {
     */
     return result;
 }
-static struct Link*GetLinks(struct Frame*frame,uint16_t version,uint16_t section){
-    struct Standard*this; 
-    for(this=frame->Standards;this&&!(this->Version==version&&this->Section==section);this=this->Previous);
-    return this->Links;
-}
-static inline int RegisterLink(struct Link*links,uint16_t ID,unsigned char*Data ){
-    struct Link*link=waitForMemory(sizeof(struct Link));
-    if(!link)return 0;
-    link->ID=ID;
-    link->Data=Data;
-    link->Next=link->Previous=NULL; 
-    if(links){
-        links->Next=link;
-        link->Previous=links;
-    }
-    links=link;
-    return 1;
-}
-static inline struct Link* GetLink(struct Link* links, uint16_t ID) {
-    struct Link* this = links;
-    for (; this && this->ID != ID; this = this->Previous);
-    return this;
-}
-int InitThread(void *data) {
-    pr_info("Initialization thread is running!\n");
 
-
-
-    pr_info("Initialization thread is exiting.\n");
-    return 0;  // Thread exits
-}
-#include <linux/proc_fs.h>
-#include <linux/fs.h>
-#include <linux/uaccess.h>
-static uint64_t threadsmax = 200, threads = 0;
-static inline int CreateThread(void *data, int (*thread_fn)(void *), const char *thread_name) {
-    if(threads>threadsmax){
-        thread_fn(data);
-        return 1;
-    }
-    return !IS_ERR_OR_NULL(kthread_run(thread_fn, data, thread_name));
-}
-static inline int RunThread(void*data){
-    struct Job*jobs=(struct Job*)data;
-    if(jobs&&threads<threadsmax)threads++;
-    if(!jobs)goto Exit;
-    while(jobs->Previous)jobs=jobs->Previous;
-    while(jobs){
-        if(!jobs->JobFunction(jobs->Data)){
-            ClosesJobs(jobs);
-            goto Exit;
-        }
-        if((jobs=jobs->Next)&&jobs->Previous){
-            kfree(jobs->Previous);
-            jobs->Previous=NULL;
-        }
-    }
-    Exit:
-    threads--;
-    return 1;
-}
-static inline int RunJobs(struct Job* Jobs){
-    return CreateThread(Jobs,RunThread,"RunJobs");
-}
-static inline int ClosesJobs(struct Job*jobs){
-    if (!jobs) return 1; 
-    if(jobs->Next)ClosesJobs(jobs->Next);
-    if(jobs->Previous)ClosesJobs(jobs->Previous);
-    kfree(jobs);
-    return 1;
-}
-static inline int CreateJob(int (*task_fn)(void *), void *data,struct Job* jobs){
-    struct Job*job=waitForMemory(sizeof(struct Job));
-    if(!job){
-        ClosesJobs(jobs);
-        return 0;
-    }
-    while(jobs&&jobs->Next)jobs=jobs->Next;
-    job->Data=data;
-    job->JobFunction=task_fn;
-    job->Next=NULL;
-    job->Previous=jobs;
-    jobs=job;
-    return 1;
-}
+static struct task_struct*BackgroundDeletionThread;
 static int __init wms_init(void){
-    char buf[16] = {0};
-    struct file *f= filp_open("/proc/sys/kernel/threads-max", O_RDONLY, 0);
-    if (!IS_ERR(f)) {
-        if (kernel_read(f,buf,sizeof(buf)-1,&pos)>0&&!kstrtoull(buf,10, &threadsmax)) 
-            threadsmax = threadsmax / 2;
-        filp_close(f, NULL);
-    }
+
+    BackgroundDeletionThread = kthread_run(BackgroundDeletion, NULL, "BackgroundDeletion");
+  
     IsServerClose = false;
     dev_add_pack(&Gateway);
     return 0;
@@ -380,6 +226,7 @@ static void __exit wms_exit(void){
     dev_remove_pack(&Gateway);
     IsServerClose=true;
     while(Frames)msleep(100); 
+    kthread_stop(BackgroundDeletionThread);
 }
 module_exit(wms_exit);
 MODULE_INFO_SETUP("Pirasath Luxchumykanthan","WeMakeSoftware Kernel Network","1.0");
--- WeMakeSoftware.h
+++ WeMakeSoftware.h
@@ -10,49 +10,10 @@ MODULE_LICENSE("GPL");\
 MODULE_AUTHOR(author);\
 MODULE_DESCRIPTION(description);\
 MODULE_VERSION(version);
-struct Link {
-    struct Link *Previous, *Next;
-    uint16_t ID; 
-    unsigned char*Data; 
-};
-struct Standard{
-    struct Standard*Previous,*Next;
-    uint16_t Version,Section;
-    bool IsDynamic;
-    struct Link*Links;
-    unsigned char*Data;
-};
-struct Status
-{
-    unsigned char Request:4,Response:4;
-};
 struct Frame{
     struct Frame*Previous,*Next;
     struct sk_buff*skb; 
-    int id;
-    struct Status Status; 
-    ktime_t Start, End;
+    int id,IsDeleted;
+    ktime_t Start;
     unsigned char*IEE802Buffer;
-    struct Standard*Standards;
-};
-enum ThreadProcess{
-    ThreadProcessWait,
-    ThreadProcessRunning,
-    ThreadProcessClose
-};
-
-struct ThreadJob {
-    struct ThreadTask*Previous,*Next;
-    struct task_struct*Thread;
-};
-
-struct Job {
-    struct Job *Previous, *Next; 
-    void *Data;
-    int (*JobFunction)(void*);
-};
-struct ThreadToDo {
-    struct ThreadToDo *Previous, *Next;
-    struct Job *Jobs;
-    
 };
