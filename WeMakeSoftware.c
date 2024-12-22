4b7c275596d1447599857ca643e2dad60be2fa90
root <root@B873EE7>
2024-12-22 11:47:57 +0000

    Auto commit

--- WeMakeSoftware.c
+++ WeMakeSoftware.c
@@ -4,15 +4,7 @@
 #include <linux/skbuff.h>
 #include <linux/workqueue.h>
 #include <linux/slab.h>
-//  memset(header, 0, len); // This is a good practice to clear the memory.
 
-
-
-// Incoming : RFC 1035
-// Useful links:
-// - https://www.rfc-editor.org/rfc/rfc1035
-// We already know its for DNS in RFC 768
-// We stop the process if the length of the packet is less than 20 bytes. (We need to change this rules when we go deeper)
 static void RFC1035Reader(
     struct sk_buff*in,
     unsigned char*dataLinkLayer,
@@ -56,10 +48,6 @@ static void StartBackgroundApplicationLayerTask(
     task->callback=callback;
     queue_work(BackgroundApplicationLayerWorkQueue,&task->work);
 }
-// Incoming : RFC 768
-// Useful links:
-// - https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xhtml
-// - https://www.rfc-editor.org/rfc/rfc768.html
 static int RFC768Reader(
     struct sk_buff*skb,
     unsigned char*dataLinkLayer,
@@ -75,10 +63,6 @@ static int RFC768Reader(
     }
     return NET_RX_SUCCESS;
 }
-// Incoming : RFC 9293
-// Useful links:
-// - https://www.iana.org/assignments/service-names-port-numbers/service-names-port-numbers.xhtml
-// - https://www.rfc-editor.org/rfc/rfc9293.html
 static int RFC9293Reader(
     struct sk_buff*skb,
     unsigned char*dataLinkLayer,
@@ -93,12 +77,6 @@ static int RFC9293Reader(
     }
     return NET_RX_SUCCESS;
 }
-// Incoming : RFC 8200
-// Useful links:
-// - https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
-// - https://www.rfc-editor.org/rfc/rfc8200.html
-// We only focus on public network addresses.
-// There many protocol numbers, but we only care about 6 (rfc9293) and 17 (RFC768).
 static int RFC8200Reader(
     struct sk_buff*skb,
     unsigned char*ieee802,
@@ -106,12 +84,6 @@ static int RFC8200Reader(
     ){
     return(rfc8200Next[8]==252||rfc8200Next[8]==253||(rfc8200Next[8]==254&&(rfc8200Next[9]>=128&&rfc8200Next[9]<=191))||rfc8200Next[8]==255)?NET_RX_SUCCESS:rfc8200Next[6]==6?RFC9293Reader(skb,ieee802,rfc8200Next,rfc8200Next+40):rfc8200Next[6]==17?RFC768Reader(skb,ieee802,rfc8200Next,rfc8200Next+40):NET_RX_SUCCESS;
 }
-// Incoming : RFC 791
-// Useful links:
-// - https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
-// - https://www.rfc-editor.org/rfc/rfc791
-// We only focus on public network addresses.
-// There many protocol numbers, but we only care about 6 (rfc9293) and 17 (RFC768).
 static int RFC791Reader(
     struct sk_buff*skb,
     unsigned char*dataLinkLayer,
@@ -119,21 +91,6 @@ static int RFC791Reader(
     ){
     return(rfc791Next[12]==172&&rfc791Next[13]>=16&&rfc791Next[13]<=31)||(rfc791Next[16]==172&&rfc791Next[17]>=16&&rfc791Next[17]<=31)||(rfc791Next[12]==192&&rfc791Next[13]==168)||(rfc791Next[16]==192&&rfc791Next[17]==168)||(rfc791Next[12]==127)||(rfc791Next[16]==127)||(rfc791Next[12]==169&&rfc791Next[13]==254)||(rfc791Next[16]==169&&rfc791Next[17]==254)||(rfc791Next[12]==0)||(rfc791Next[16]==0)?NET_RX_SUCCESS:rfc791Next[9]==6?RFC9293Reader(skb,dataLinkLayer,rfc791Next,rfc791Next+((rfc791Next[0]&15)*4)):rfc791Next[9]==17?RFC768Reader(skb,dataLinkLayer,rfc791Next,rfc791Next+((rfc791Next[0]&15)*4)):NET_RX_SUCCESS;
 }
-// Incoming : RFC 826 
-// Sender : IEEE802_3Reader
-// Useful links:
-// - https://www.rfc-editor.org/rfc/rfc826
-// - https://www.iana.org/assignments/arp-parameters/arp-parameters.xhtml
-//
-// Note : Do not return NET_RX_DROP in the RFC826 handling step, as it will prevent 
-// other software from obtaining the MAC address. Only listen to ARP requests.
-//
-// This function ensures compliance with the rule that we do not deny the packet; 
-// we simply return NET_RX_SUCCESS by default. 
-//
-// What can we use this for? Not much, except if the router restarts or undergoes new configuration changes.
-// We can make use of it by creating a global variable, which we can set `RouterMacAddress`.
-// If all 6 bytes are zero, then it's the local MAC address; if not, then it's the router MAC address.
 static int RFC826Reader(
     struct sk_buff*skb,
     unsigned char*ieee802SourceMediaAccessControlAddress
@@ -141,7 +98,6 @@ static int RFC826Reader(
 
     return NET_RX_SUCCESS;
 }
-// Incoming : IEEE 802.3
 static int DataLinkLayerReader(
     struct sk_buff*skb,
     struct net_device*dev,
